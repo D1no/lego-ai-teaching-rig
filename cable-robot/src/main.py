ebc0935a76004618c09081786c9fe0af0c22b226
Dino Scheidt <github@dino-scheidt.de>
2024-01-11 04:11:47 +0100

    📝 Add Instruction for AI Assistants. ChatGPT fail: https://chat.openai.com/share/429be807-14b4-41ec-b151-0446e23b0198

--- cable-robot/src/main.py
+++ cable-robot/src/main.py
@@ -1,3 +1,6 @@
+# Description for AI Assistance
+# The following code is used to control a cable robot made out of lego technic and motors with position encoders by using a library called "pybricks" which runs on the lego mindstorms robot inventor hub. This robot is a parallel robot meaning, that it has four motors arranged in rectangular arrangement. These four motors sit in each corner of the rectangle and are connected to each other with a string of yarn. The top left motor is directly tied to the motor in the bottom right, and the motor in the top right is directly connected with the motor in the bottom left. The two crossing strings are connected with a knot to a disc. This means, that the motors can position this disc in a x and y coordinate system by pulling their end of the string with varying degrees to their corner. Each motor has a spring and a clutch for holding the string in tensioning while not overloading the motor by accident. In the beginning, it is unknown if the rectangular arrangement is square and how large it is. Therefore the range of travel is unknown. In the beginning, the string connecting this arrangement is also not tensioned and not in equal length to each motor. The goal of this code is to calibrate this tension array by finding valid travel ranges for each motor and then use this information to control the robot in a cartesian coordinate system going from 0 to 100 for each axis and is a safe zone of about 80% of the full size of the available space. The first goal is to move the disc to the center of that space. Next, move the disc clockwise to each corner in a circular pattern. Make sure that at any given moment, no motor is running faster than the MAX_SPEED_ANGLE_PER_SEC. The STALL_CLUTCH_DUTY_LIMIT is the value at which a motor stalls but still puts it end of the string under tension with its spring without triggering the safety clutch. This is useful for detecting limits by using the run_until_stalled method. The motor.angle() method provides an absolute angle the motor is at during its life. It is read from the motor encoder and is an absolute integer between that can be negative. It is useful to find and track travel ranges in each direction. Which direction of the motor winds in the string is written below. So keep in mind, that travel from the motors perspective can be flipped. The documentation for the pybricks library can be found here: https://docs.pybricks.com/en/latest/index.html where the relevant page for the motors is this page https://docs.pybricks.com/en/latest/pupdevices/motor.html and for multi tasking this page https://docs.pybricks.com/en/latest/tools/index.html .
+
 from pybricks.hubs import InventorHub
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.pupdevices import ColorSensor, Motor, UltrasonicSensor
@@ -10,54 +13,105 @@ hub.light.blink(Color.GREEN, [500, 500])
 
 # Model Constants
 STALL_CLUTCH_DUTY_LIMIT = 20
-MAX_SPEED_ANGLE_PER_SEC = 1500
+MAX_SPEED_ANGLE_PER_SEC = 1000
 
+###########################################################
 # Motors
-motor_tl = Motor(Port.A)
-motor_br = Motor(Port.F)
+###########################################################
 
-motor_tr = Motor(Port.B)
-motor_bl = Motor(Port.E)
+# ---------------------------------------------------------
+# ↖ Top Left
+top_left = Motor(Port.A)
+
+# Direction of the motor
+top_left_in = -1
+top_left_out = top_left_in * -1
+
+top_left_travel_min = None
+top_left_travel_max = None
+top_left_travel_mid = None
+top_left_travel_left_neighbor = None
+top_left_travel_right_neighbor = None
+
+# ---------------------------------------------------------
+# ↘ Bottom Right
+bottom_right = Motor(Port.F)
+
+# Direction of the motor
+bottom_right_in = -1
+bottom_right_out = bottom_right_in * -1
+
+# Travel Range
+bottom_right_travel_min = None
+bottom_right_travel_max = None
+bottom_right_travel_mid = None
+bottom_right_travel_left_neighbor = None
+bottom_right_travel_right_neighbor = None
+
+# ---------------------------------------------------------
+# ↗ Top Right
+top_right = Motor(Port.B)
+
+# Direction of the motor
+top_right_in = 1
+top_right_out = top_right_in * -1
+
+# Travel Range
+top_right_travel_min = None
+top_right_travel_max = None
+top_right_travel_mid = None
+top_right_travel_left_neighbor = None
+top_right_travel_right_neighbor = None
+
+# ---------------------------------------------------------
+# ↙ Bottom Left
+bottom_left = Motor(Port.E)
+
+# Direction of the motor
+bottom_left_in = 1
+bottom_left_out = bottom_left_in * -1
+
+# Travel Range
+bottom_left_travel_min = None
+bottom_left_travel_max = None
+bottom_left_travel_mid = None
+bottom_left_travel_left_neighbor = None
+bottom_left_travel_right_neighbor = None
 
 
-# Start moving at 300 degrees per second.
-
-# motor_tl.run(300)
-# motor_br.run(300)
+###########################################################
+# Solving Stages
+###########################################################
 
 
 # Stage 1: Make sure everything is in tension
-async def bring_under_tension():
+async def bring_under_tension(speed: int):
     await multitask(
-        motor_tl.run_until_stalled(
-            speed=-300, then=Stop.HOLD, duty_limit=STALL_CLUTCH_DUTY_LIMIT
+        top_left.run_until_stalled(
+            speed=speed * top_left_in,
+            then=Stop.HOLD,
+            duty_limit=STALL_CLUTCH_DUTY_LIMIT,
         ),
-        motor_br.run_until_stalled(
-            speed=-300, then=Stop.HOLD, duty_limit=STALL_CLUTCH_DUTY_LIMIT
+        bottom_right.run_until_stalled(
+            speed=speed * bottom_right_in,
+            then=Stop.HOLD,
+            duty_limit=STALL_CLUTCH_DUTY_LIMIT,
         ),
-        motor_tr.run_until_stalled(
-            speed=300, then=Stop.HOLD, duty_limit=STALL_CLUTCH_DUTY_LIMIT
+        top_right.run_until_stalled(
+            speed=speed * top_right_in,
+            then=Stop.HOLD,
+            duty_limit=STALL_CLUTCH_DUTY_LIMIT,
         ),
-        motor_bl.run_until_stalled(
-            speed=300, then=Stop.HOLD, duty_limit=STALL_CLUTCH_DUTY_LIMIT
+        bottom_left.run_until_stalled(
+            speed=speed * bottom_left_in,
+            then=Stop.HOLD,
+            duty_limit=STALL_CLUTCH_DUTY_LIMIT,
         ),
     )
 
 
-run_task(bring_under_tension())
+# run_task(bring_under_tension(300))
 
-# Display the angle and speed 50 times.
-for i in range(100):
-    # Read the angle (degrees) and speed (degrees per second).
-    angle = motor_tl.angle()
-    speed = motor_tl.speed()
-
-    # Print the values.
-    print(angle, speed)
-    # print("Top Left Tension:", tl_tension)
-    # print("Bottom Right Tension:", br_tension)
-    # print("Top Right Tension:", tr_tension)
-    # print("Bottom Left Tension:", bl_tension)
-
-    # Wait some time so we can read what is displayed.
-    wait(200)
+###########################################################
+# ChatGPTs Attempt
+###########################################################
